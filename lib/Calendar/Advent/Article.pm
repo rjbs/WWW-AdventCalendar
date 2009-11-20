edd5551f3106b190af42cbda0972b7383a0f0f3c
Ricardo Signes <rjbs@cpan.org>
2009-11-20 14:34:32 -0500

    ...done? except for feed?

--- lib/Calendar/Advent/Article.pm
+++ lib/Calendar/Advent/Article.pm
@@ -1,8 +1,31 @@
 package Calendar::Advent::Article;
 use Moose;
 
+use autodie;
+use Pod::Xhtml;
+
 has body  => (is => 'ro', isa => 'Str',      required => 1);
 has title => (is => 'ro', isa => 'Str',      required => 1);
 has date  => (is => 'ro', isa => 'DateTime', required => 1);
+
+sub body_xhtml {
+  my ($self) = @_;
+
+  my $body = $self->body;
+  open my $fh, '<', \$body;
+
+  my $px = Pod::Xhtml->new(
+    FragmentOnly => 1,
+    MakeIndex    => 0,
+    MakeMeta     => 0,
+    StringMode   => 1,
+    TopLinks     => 0,
+  );
+  $px->parse_from_filehandle($fh);
+
+  my $string = $px->asString;
+
+  return $string;
+}
 
 1;
