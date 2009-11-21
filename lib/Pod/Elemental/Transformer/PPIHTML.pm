952515332016c4867ded96317d322cb0d6865780
Ricardo Signes <rjbs@cpan.org>
2009-11-20 23:58:24 -0500

    fix formatting and escaping of sequences

--- lib/Pod/Elemental/Transformer/PPIHTML.pm
+++ lib/Pod/Elemental/Transformer/PPIHTML.pm
@@ -23,19 +23,35 @@ sub transform_node {
 
     my @lines = split m{<br>\n?}, $html;
 
-    $html = "<table class='ppi-html'>"
+    my $space_count = 2 + length scalar @lines;
+    my $spc = ' ' x $space_count;
+
+    $html = "<table class='ppi-html'>\n\n\n"
+          . "<tr class='line'><td><span class='line_number'>$spc</span>&nbsp;</td></tr>\n"
           . (join q{}, map {; "<tr class='line'><td>$_</td></tr>\n" } @lines)
+          . "<tr class='line'><td><span class='line_number'>$spc</span>&nbsp;</td></tr>\n"
           . "</table>";
 
+    # This should not be needed, because this is a data paragraph, not a
+    # ordinary paragraph, but Pod::Xhtml doesn't seem to know the difference
+    # and tries to expand format codes. -- rjbs, 2009-11-20
+    $html =~ s/^/  /gsm;
+
     my $para = Pod::Elemental::Element::Pod5::Data->new({
-      content => "<div class='ppi-html'>$html</div>\n",
+      content => $html,
+    });
+
+    # This should not be needed, because if there's a \n\n in the content, we
+    # should get a =begin and not a =for. -- rjbs, 2009-11-20
+    my $hack = Pod::Elemental::Element::Pod5::Data->new({
+      content => "<!-- hack -->\n",
     });
 
     my $new = Pod::Elemental::Element::Pod5::Region->new({
       format_name => 'xhtml',
       is_pod      => 0,
       content     => '',
-      children    => [ $para ],
+      children    => [ $para, $hack ],
     });
 
     $parent_node->children->[ $i ] = $new;
