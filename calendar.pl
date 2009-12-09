01ca4e661840f6b1140c377edc68571e2f4a25ea
Ricardo Signes <rjbs@cpan.org>
2009-12-09 10:30:33 -0500

    track heads usefully

--- calendar.pl
+++ calendar.pl
@@ -9,13 +9,13 @@ use lib '/Users/rjbs/code/hub/list-cell/lib';
 {
   package Calendar;
   use Moose;
-  with 'List::Cell';
 
-  has date => (is => 'ro', isa => 'DateTime', required => 1);
-
-  sub day_of_week {
-    return $_[0]->date->day_of_week % 7
-  }
+  has first_cell => (
+    is  => 'ro',
+    isa => 'Calendar::Cell',
+    writer   => '_set_first_cell',
+    required => 1,
+  );
 
   sub BUILD {
     my ($self) = @_;
@@ -25,32 +25,45 @@ use lib '/Users/rjbs/code/hub/list-cell/lib';
 
   sub _ensure_start_sunday {
     my ($self) = @_;
-    my $class = ref $self;
 
-    my $first = $self->first;
+    my $first = $self->first_cell;
 
     while ($first->day_of_week != 0) {
       my $prev_date = $first->date - DateTime::Duration->new(days => 1);
-      my $prev_cell = $class->new({ date => $prev_date });
+      my $prev_cell = Calendar::Cell->new({ date => $prev_date });
 
       say "moving " . $prev_cell->date->ymd . " to head";
-      $prev_cell->replace_next($first);
+      $first->replace_prev($prev_cell);
 
       $first = $first->first;
     }
+
+    $self->_set_first_cell($first) if $first != $self->first_cell;
   }
 
   sub _ensure_end_saturday {
   }
 }
 
+{
+  package Calendar::Cell;
+  use Moose;
+  with 'List::Cell';
+
+  has date => (is => 'ro', isa => 'DateTime', required => 1);
+
+  sub day_of_week {
+    return $_[0]->date->day_of_week % 7
+  }
+}
+
 my $cal = Calendar->new({
-  date => DateTime->now(time_zone => 'local')
+  first_cell => Calendar::Cell->new({
+    date => DateTime->now(time_zone => 'local')
+  }),
 });
 
-warn $cal->prev->date->ymd;
-
-for (my $cell = $cal->first; $cell; $cell = $cell->next) {
+for (my $cell = $cal->first_cell; $cell; $cell = $cell->next) {
   printf "%2s: %s\n", $cell->day_of_week, $cell->date->ymd;
 }
 
