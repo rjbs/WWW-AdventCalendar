16995dde2dd6c3f2df337c96bc8639eb8b2bbf6e
Ricardo Signes <rjbs@cpan.org>
2009-12-10 22:48:10 -0500

    making xhtml pod formatter saner?

--- lib/Pod/AdventXHTML.pm
+++ lib/Pod/AdventXHTML.pm
@@ -1,5 +1,5 @@
 use strict;
-package Pod::AdventXHTML;
+package Pod::Simple::XHTML::WithXHTMLRegions;
 use base 'Pod::Simple::XHTML';
 
 sub new {
@@ -11,28 +11,42 @@ sub new {
   return $parser;
 }
 
+# Absolutely, without a doubt, the wrong place to do this...
+# ...but it works. -- rjbs, 2009-12-10
 sub _ponder_Data {
   print {$_[0]->{'output_fh'}} $_[1][2], "\n\n";
 }
 
 sub handle_text {
-  $_[0]{'scratch'} .= ($_[0]->{target}[-1]||'') eq 'xhtml'
-                    ? '' # ?!?!?!? SHOULD NOT HAPPEN
-                    : HTML::Entities::encode_entities( $_[1] )
+  my $self = shift;
+
+  # I don't understand why I can't just tack on the contents un-HTML-encoded
+  # when we're in an XHTML region.  I can't, though.  All kinds of crazy crap
+  # happens. -- rjbs, 2009-12-10
+  return if $self->{__in_advent_xhtml};
+  
+  $self->SUPER::handle_text(@_);
 }
 
 sub start_for {
   my ($self, $flags) = @_;
 
-  Carp::confess("non-xhtml target section begun")
+  return $self->SUPER::start_for($flags)
     unless $flags->{target_matching} eq 'xhtml';
 
-  push @{ $self->{target} ||= [] }, $flags->{target_matching};
+  Carp::confess("xhtml target sections may not nest")
+    if $self->{__in_advent_xhtml};
+
+  $self->{__in_advent_xhtml} = 1;
 }
 
 sub end_for {
   my ($self, $flags) = @_;
-  pop @{ $self->{target} ||= [] };
+
+  return $self->SUPER::start_for($flags)
+    unless $flags->{target_matching} eq 'xhtml';
+
+  $self->{__in_advent_xhtml} = 0;
 }
 
 sub _end_head {
